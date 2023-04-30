fbda8d21a70483662dc63db2ebfe703e989aaf4e
JK-RCZ <jankarchewski@outlook.com>
2023-04-30 22:21:54 +0300

    121

--- afterparty.sh
+++ afterparty.sh
@@ -8,15 +8,9 @@ echo $COUNT
 if [ $COUNT == 35 ]
             then
             echo -e "FUCK CHECK SUCCESSFULL!"
-            rm /home/ec2-user/result.txt
-            rm /home/ec2-user/count.txt
-            rm /home/ec2-user/count2.txt
             exit 0
 
             else
             echo -e "FUCK CHECK FAILED!"
-            rm /home/ec2-user/result.txt
-            rm /home/ec2-user/count.txt
-            rm /home/ec2-user/count2.txt
             exit 1
             fi
