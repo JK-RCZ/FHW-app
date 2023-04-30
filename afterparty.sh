d46aeb2a036d9fac99effad5df09f32a147e8e91
JK-RCZ <jankarchewski@outlook.com>
2023-04-30 22:28:03 +0300

    1123

--- afterparty.sh
+++ afterparty.sh
@@ -3,7 +3,8 @@
 
 grep -i fuck /home/ec2-user/result.txt > /home/ec2-user/count.txt
 wc -l /home/ec2-user/count.txt > /home/ec2-user/count2.txt
-COUNT=$(grep -Eo "[0-9]{1,4}" /home/ec2-user/count2.txt)
+grep -Eo "[0-9]{1,4}" /home/ec2-user/count2.txt > /home/ec2-user/count3.txt
+COUNT=$(awk '{print $1}' /home/ec2-user/count3.txt)
 echo $COUNT
 if [ $COUNT == 35 ]
             then
