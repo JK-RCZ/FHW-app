e89c764f10e72aa17c11586daf1356f89e09f9c0
JK-RCZ <jankarchewski@outlook.com>
2023-04-30 22:49:03 +0300

    11

--- afterparty.sh
+++ afterparty.sh
@@ -5,7 +5,6 @@ grep -i fuck /home/ec2-user/result.txt > /home/ec2-user/count.txt
 wc -l /home/ec2-user/count.txt > /home/ec2-user/count2.txt
 grep -Eo "[0-9]{1,4}" /home/ec2-user/count2.txt > /home/ec2-user/count3.txt
 COUNT=$(sed 's/  */\t/g' /home/ec2-user/count2.txt | cut -f1)
-echo $COUNT
 if [ $COUNT == 35 ]
             then
             echo -e "FUCK CHECK: $COUNT OUT OF 35 - SUCCESSFULL!"
