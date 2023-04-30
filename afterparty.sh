edbc261ab718538c3a0938a7001b5264ac060736
JK-RCZ <jankarchewski@outlook.com>
2023-04-30 22:01:08 +0300

    1121

--- afterparty.sh
+++ afterparty.sh
@@ -2,16 +2,20 @@
 
 
 grep -i fuck /home/ec2-user/result.txt > /home/ec2-user/count.txt
-wait 1
 wc -l count.txt > /home/ec2-user/count2.txt
-wait 1
 COUNT=$(grep -Eo "[0-9]{1,4}" count2.txt)
 echo $COUNT
 if [ $COUNT == 35 ]
             then
             echo -e "FUCK CHECK SUCCESSFULL!"
+            rm /home/ec2-user/result.txt
+            rm /home/ec2-user/count.txt
+            rm /home/ec2-user/count2.txt
             exit 0
             else
             echo -e "FUCK CHECK FAILED!"
+            rm /home/ec2-user/result.txt
+            rm /home/ec2-user/count.txt
+            rm /home/ec2-user/count2.txt
             exit 1
             fi
