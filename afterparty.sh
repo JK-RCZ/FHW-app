ac378ba20dda6769d037b31f4279da0f4ad744ed
JK-RCZ <jankarchewski@outlook.com>
2023-04-30 21:56:05 +0300

    1

--- afterparty.sh
+++ afterparty.sh
@@ -2,7 +2,9 @@
 
 
 grep -i fuck /home/ec2-user/result.txt > /home/ec2-user/count.txt
+wait 1
 wc -l count.txt > /home/ec2-user/count2.txt
+wait 1
 COUNT=$(grep -Eo "[0-9]{1,4}" count2.txt)
 echo $COUNT
 if [ $COUNT == 35 ]
